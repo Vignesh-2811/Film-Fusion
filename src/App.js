ae7a2427cb1920a5acc55708344b527ceff90312
Vignesh-2811 <shakthivignesh2002@gmail.com>
2023-05-05 21:08:07 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='h-header bg-main w-full'>
+      <h1 className='text-h1 text-star font-bold underline'>Hello World!</h1>
+      <p className='text-white'>lorem</p>
     </div>
   );
 }
