808a7e56392c5be0bf367d21f4c18eb97210ae55
Vignesh-2811 <shakthivignesh2002@gmail.com>
2023-05-06 16:55:20 +0530

    Contact Us

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { Route, Routes } from 'react-router-dom';
 import HomeScreen from './Screens/HomeScreen';
 import AboutUs from './Screens/AboutUs';
 import NotFound from './Screens/NotFound';
+import ContactUs from './Screens/ContactUs';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<HomeScreen />} />
       <Route path='/about-us' element={<AboutUs />} />
+      <Route path='/contact-us' element={<ContactUs />} />
+
       <Route path='*' element={<NotFound />} />
     </Routes>
   );
