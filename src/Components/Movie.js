be4433831402896ebeafa6d69aff9a5b45cba2b8
Vignesh-2811 <shakthivignesh2002@gmail.com>
2023-05-07 07:51:53 +0530

    Single movie page

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -6,7 +6,7 @@ function Movie({ movie }) {
   return (
     <>
       <div className='border border-border p-1 hover:scale-95 transition relative rounded overflow-hidden'>
-        <Link to={`/movies/${movie?.name}`} className='w-full'>
+        <Link to={`/movie/${movie?.name}`} className='w-full'>
           <img
             src={require(`/src/images/${movie.image}`)}
             alt={movie?.name}
---
+++ src/Data/UserData.js
@@ -0,0 +1,22 @@
+export const UsersData = [
+  {
+    name: 'Michael B. Jordan',
+    image: 'c1.png',
+  },
+  {
+    name: 'Vin Diesel',
+    image: 'c2.png',
+  },
+  {
+    name: 'Morgan Freeman',
+    image: 'c3.png',
+  },
+  {
+    name: 'Emilia Clarke',
+    image: 'c4.png',
+  },
+  {
+    name: 'Will Smith',
+    image: 'c5.jpeg',
+  },
+];
